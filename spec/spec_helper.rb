06a95f75a917c33cdbccd54133f2c5aec45fdf38
Ryan Hurtis <rhurtis@hotmail.com>
2022-01-29 18:18:39 -0500

    using the simplecov json formatter.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,8 +21,10 @@ SimpleCov.start
 # execute the following only when running Github Actions CI.
 if ENV['CI'] == true
   # Formatting for codecov
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  # require 'codecov'
+  # SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  require 'simplecov-json'
+  SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
 end
 
 
