3a721f6dca0360d6ef432f0e036e1f6cf7c557ca
Ryan Hurtis <rhurtis@hotmail.com>
2022-01-30 00:08:37 -0500

    Using simplecov corbetura formatter to generate an xml

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,17 +18,18 @@
 require 'simplecov'
 SimpleCov.start
 
-# execute the following only when running Github Actions CI.
-if ENV['CI'] == true
-  # Formatting for codecov
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-  # require 'simplecov-json'
-  # SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
+require 'simplecov-cobertura'
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 
-  # require 'simplecov-cobertura'
-  # SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
-end
+# execute the following only when running Github Actions CI.
+# if ENV['CI'] == true
+#   # Formatting for codecov
+#   require 'codecov'
+#   SimpleCov.formatter = SimpleCov::Formatter::Codecov
+#   # require 'simplecov-json'
+#   # SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
+
+# end
 
 
 
