ceca8b405b60ca8748fe72a68042b6c97adfa1fb
Ryan Hurtis <rhurtis@hotmail.com>
2022-02-05 12:57:16 -0500

    Modified the articles_controller to include methods for crud operations.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,28 @@ class ArticlesController < ApplicationController
         @article = Article.all
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+
+        @article.destroy
+
+        redirect_to articles_path, status: :see_other 
+    end
+
     private
     def article_params
         params.require(:article).permit(:title, :body)
