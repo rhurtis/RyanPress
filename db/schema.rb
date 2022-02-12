78d2c62deab2fbfd7af9c2fac42be10b6f4fd07f
Ryan Hurtis <rhurtis@hotmail.com>
2022-02-12 16:04:52 -0500

    Modified schema.rb to include validations in the database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_003200) do
+ActiveRecord::Schema.define(version: 2022_02_06_012451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
+    t.string "title", null: false
+    t.string "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["title"], name: "index_articles_on_title", unique: true
   end
 
 end
