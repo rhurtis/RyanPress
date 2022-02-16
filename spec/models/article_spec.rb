6a82c2ae4b57cf988335b77a0825875efce81a65
Ryan Hurtis <rhurtis@hotmail.com>
2022-02-16 16:25:43 -0500

    Modified the models spec to utilize factory bot.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,19 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
     it "is valid with a title and body" do
-        article = Article.create(title:"Test Title", body: "Test Body")
+        article = create(:article_with_title_and_body)
         expect(article).to be_valid
     end
 
     context "is invalid" do
         it "without a title" do
-            article = Article.create(body: "Test Body")
-            expect(article).to_not be_valid
+            article = build(:article_without_title)
+            article.valid?
+            expect(article.errors[:title]).to include("can't be blank")
         end
 
         it "without a body" do
-            article = Article.create(title: "Test Title")
-            expect(article).to_not be_valid
+            article = build(:article_without_body)
+            article.valid?
+            expect(article.errors[:body]).to include("can't be blank")
         end
     end
 
