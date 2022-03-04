af5853f1fbea38d9fb8e61740011cf74975d73bc
Ryan Hurtis <rhurtis@hotmail.com>
2022-03-04 16:52:21 -0500

    Modified the article model to create/ update a slug for each entry.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,25 @@
 class Article < ApplicationRecord
     validates :title, presence: true, uniqueness: true
     validates :body, presence: true
+    validates :slug, uniqueness: true
+    
+    after_create :update_slug
+    before_update :assign_slug
+
+    def update_slug
+        update_attribute(:slug, assign_slug)
+    end
+
+    def create_slug
+        return self.title.parameterize
+    end
+
+    def to_param
+       slug
+    end
+
+    private
+    def assign_slug
+        self.slug = create_slug
+    end
 end
