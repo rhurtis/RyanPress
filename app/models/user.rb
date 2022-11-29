8e6fdab37eb50fcf3866ee48b3f4f7f12c0a2e80
Ryan H <58322073+rhurtis@users.noreply.github.com>
2022-11-28 21:53:13 -0500

    Modified models/user.rb to have encrypts and validates commands on separate lines, to prevent errors.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-    validates encrypts :email, presence: true, uniqueness: true, deterministic: true
-    validates encrypts :password, presence: true
+    encrypts :email, deterministic: true, downcase: true
+    validates :email, presence: true, uniqueness: true
+    encrypts :password
+    validates  :password, presence: true
 end
