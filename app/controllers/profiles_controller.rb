0af519409073d539366550b71c41c9162d652ac4
Ryan H <58322073+rhurtis@users.noreply.github.com>
2023-02-06 21:40:30 -0500

    Modified profiles_controller.rb: Added profile params and redirect to root path.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,18 @@ class ProfilesController < ApplicationController
         @profile = Profile.new
     end
 
+    def create
+        @profile = Profile.new(profile_params)
+        if @profile.save
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+      end
+
+      private
+      def profile_params
+          params.require(:profile).permit(:username, :user_id)
+      end
+
 end
