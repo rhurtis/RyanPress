599846023cdb67ab644c1a0ceb8819ec17133191
Ryan H <58322073+rhurtis@users.noreply.github.com>
2023-02-22 22:14:03 -0500

    Added redirect to root on login. Rerendering the new template when the incorrect credentials are supplied.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,16 @@
 class SessionsController < ApplicationController
 
-
     def new
-        
+
     end
 
     def create
         user = User.find_by_email(params[:email])
-        if user.authenticate(params[:password])
+        if user && user.authenticate(params[:password])
             session[:user_id] = user.id
+            redirect_to "/"
         else
-            
+            redirect_to "/sessions/new"
         end
     end
-
-    
-
 end
--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -3,15 +3,21 @@ require 'rails_helper'
 RSpec.describe "Session", type: :request do
 
     describe  'POST /sessions' do
-        it "takes correct user credentials and creates a session" do
+        it "takes correct user credentials, creates a session and redirects to the home page" do
             test_user = User.create(email: "ryan@example.com", password: "123456")
 
             post "/sessions", :params => {email: "ryan@example.com" , password: "123456"}
 
             expect(session[:user_id]).to eq(test_user.id)
+            expect(response).to redirect_to "/"
         end
-        
 
+        it "rerenders the sessions/new template when the users credentials are incorrect" do
+            post "/sessions", :params => {email: "ryan@example.com" , password: "123456"}
+
+            expect(session[:user_id]).to eq(nil)
+            expect(response).to redirect_to "/sessions/new"
+        end
     end
 
     describe "GET /sessions/new" do
